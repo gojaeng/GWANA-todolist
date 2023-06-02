45b6b91dda361ba7c1f0037a07897721047e0f9d
gojaeng <gojh1013@gmail.com>
2023-06-02 20:34:35 +0900

    List 부분 수정 중

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -2,31 +2,34 @@ import React from "react";
 import styled from "styled-components";
 
 const TodoHeadBlock = styled.div`
-    padding: 48px, 32px, 32px, 24px;
-    border-bottom: 1px solid #e9ecef;
-    
-    h1{
-        margin:0,
-        font-size:36px;
-        color: #343a40;
-    }
-    .day{
-        maring-top:4px;
-        color: #868e96;
-        font-size: 21px;
-    }
-    .tasks-left{
-        color: #20c997;
-        font-size:18px;
-        margin-top:40px;
-        font-weight:bold;
-    }
+  padding-top: 48px;
+  padding-left: 32px;
+  padding-right: 32px;
+  padding-bottom: 24px;
+  border-bottom: 1px solid #e9ecef;
+  h1 {
+    margin: 0;
+    font-size: 36px;
+    color: #343a40;
+  }
+  .day {
+    margin-top: 4px;
+    color: #868e96;
+    font-size: 21px;
+  }
+  .tasks-left {
+    color: #20c997;
+    font-size: 18px;
+    margin-top: 40px;
+    font-weight: bold;
+  }
 `;
 const currentDate = new Date();
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const options = { year: "numeric", month: "long", day: "numeric" };
 const formattedDate = currentDate.toLocaleDateString(undefined, options);
-const currentDayOfWeek = currentDate.toLocaleDateString(undefined, { weekday: 'long' });
-
+const currentDayOfWeek = currentDate.toLocaleDateString(undefined, {
+  weekday: "long",
+});
 
 function TodoHead() {
   return (
