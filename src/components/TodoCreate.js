34f93b16273f5a48588698bc78126da3db4574e4
gojaeng <gojh1013@gmail.com>
2023-06-03 17:33:26 +0900

    TodoList 완료

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import { MdAdd } from "react-icons/md";
+import { useTodoDispatch, useTodoNextId } from "../TodoContext";
 
 const CircleButton = styled.button`
   background: #38d9a9;
@@ -17,6 +18,7 @@ const CircleButton = styled.button`
   height: 80px;
   display: block;
   align-items: center;
+  justify-content: center;
   font-size: 60px;
   position: absolute;
   left: 50%;
@@ -34,7 +36,7 @@ const CircleButton = styled.button`
   ${(props) =>
     props.open &&
     css`
-      background: #ff6b6d;
+      background: #ff6b6b;
       &:hover {
         background: #ff8787;
       }
@@ -54,7 +56,11 @@ const InsertFormPositioner = styled.div`
 
 const InsertForm = styled.form`
   background: #f8f9fa;
-  padding: 32px 32px 72px 32px;
+  padding-left: 32px;
+  padding-top: 32px;
+  padding-right: 32px;
+  padding-bottom: 72px;
+
   border-bottom-left-radius: 16px;
   border-bottom-right-radius: 16px;
   border-top: 1px solid #e9ecef;
@@ -62,7 +68,7 @@ const InsertForm = styled.form`
 
 const Input = styled.input`
   padding: 12px;
-  border-radius: 40px;
+  border-radius: 4px;
   border: 1px solid #dee2e6;
   width: 100%;
   outline: none;
@@ -72,14 +78,39 @@ const Input = styled.input`
 
 function TodoCreate() {
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
+  const dispatch = useTodoDispatch();
+  const nextId = useTodoNextId();
+
   const onToggle = () => setOpen(!open);
+  const onChange = (e) => setValue(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault(); 
+    dispatch({
+      type: "CREATE",
+      todo: {
+        id: nextId.current,
+        text: value,
+        done: false,
+      },
+    });
+    setValue("");
+    setOpen(false);
+    nextId.current += 1;
+  };
 
   return (
     <>
       {open && (
         <InsertFormPositioner>
-          <InsertForm>
-            <Input autoFocus placeholder="할 일을 입력후, Enter를 누르세요" />
+          <InsertForm onSubmit={onSubmit}>
+            <Input
+              autoFocus
+              placeholder="할 일을 입력 후, Enter 를 누르세요"
+              onChange={onChange}
+              value={value}
+            />
           </InsertForm>
         </InsertFormPositioner>
       )}
@@ -90,4 +121,4 @@ function TodoCreate() {
   );
 }
 
-export default TodoCreate;
+export default React.memo(TodoCreate);
--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -25,21 +25,24 @@ const TodoHeadBlock = styled.div`
     font-weight: bold;
   }
 `;
-const currentDate = new Date();
-const options = { year: "numeric", month: "long", day: "numeric" };
-const formattedDate = currentDate.toLocaleDateString(undefined, options);
-const currentDayOfWeek = currentDate.toLocaleDateString(undefined, {
-  weekday: "long",
-});
 
 function TodoHead() {
   const todos = useTodoState();
-  console.log(todos);
+  const undoneTasks = todos.filter((todo) => !todo.done);
+
+  const today = new Date();
+  const dateString = today.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const dayName = today.toLocaleDateString("ko-KR", { weekday: "long" });
+
   return (
     <TodoHeadBlock>
-      <h1>{formattedDate}</h1>
-      <div className="day">{currentDayOfWeek}</div>
-      <div className="tasks-left">할 일 2개 남음</div>
+      <h1>{dateString}</h1>
+      <div className="day">{dayName}</div>
+      <div className="tasks-left">할 일 {undoneTasks.length}개 남음</div>
     </TodoHeadBlock>
   );
 }
